02db54517cfc2ddc8a1419384e9db42958fae96b
Glowman554 <55138266+Glowman554@users.noreply.github.com>
2022-07-30 20:19:18 +0200

    added cmake project

---
+++ assembler/lexer.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <stdint.h>
+#include "list.h"
+
+struct lexer_token_t
+{
+	int type;
+	int pos_start;
+	int pos_end;
+	union
+	{
+		char data_s[0xff];
+		uint16_t data_i;
+	};
+};
+
+enum lexer_token_type
+{
+	ID,
+	COLLON,
+	COMMA,
+	NUMBER,
+	LPAREN,
+	RPAREN
+};
+
+class lexer
+{
+	public:
+		lexer(char* code);
+
+		bool lex();
+
+		list<lexer_token_t>* get_tokens();
+
+	private:
+		list<lexer_token_t> tokens;
+
+		char* code;
+		int code_len;
+
+		int pos;
+		char current_char;
+
+		void advance();
+		void __id();
+		bool __nm();
+
+		bool __parse_nm(char* input, int* out);
+};
